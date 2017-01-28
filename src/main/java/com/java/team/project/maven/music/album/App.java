8919b1a5c694a2a4fae2a5987d6e3fe1d646f0ad
Maryana Udymovych <udymovych.maryana@gmail.com>
2017-01-28 21:04:55 +0200

    My first commit

---
+++ src/main/java/com/java/team/project/maven/music/album/App.java
@@ -0,0 +1,13 @@
+package com.java.team.project.maven.music.album;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
